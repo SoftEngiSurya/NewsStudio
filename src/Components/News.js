e089c9f38cdcab7c7d133656d11e9c81ce9b67fa
SoftEngiSurya <148215934+SoftEngiSurya@users.noreply.github.com>
2023-12-14 13:27:59 +0530

    all set and call the apis

---
+++ src/Components/News.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+
+  articles = []
+
+  constructor(){
+    super();
+    this.state ={
+        articles:this.articles,
+        loading:false
+    }
+}
+      async componentDidMount(){
+        let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7cfe73c637a94127bac4a1965b716113";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({articles:parsedData.articles})
+      }
+
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>News By Surya Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+
+          return  <div className="col-md-4" key={element.url}>
+        <NewsItems  title={element.title} description={element.description?element.description.slice(0,70):""} imageUrl={element.urlToImage?element.urlToImage:""} newsUrl={element.url}/>
+            </div>
+
+
+        })}
+            
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
