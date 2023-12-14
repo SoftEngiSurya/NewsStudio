f4fcd7ff03916fe7d1391cb3a46d509809d72664
SoftEngiSurya <148215934+SoftEngiSurya@users.noreply.github.com>
2023-12-14 14:19:16 +0530

    now set the api and categories

--- src/Components/News.js
+++ src/Components/News.js
@@ -13,7 +13,7 @@ export class News extends Component {
     }
 }
       async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=7cfe73c637a94127bac4a1965b716113";
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=7cfe73c637a94127bac4a1965b716113";
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData)
@@ -28,7 +28,7 @@ export class News extends Component {
         <div className="row">
         {this.state.articles.map((element)=>{
 
-          return  <div className="col-md-4" key={element.url}>
+          return  <div className="col-md-3" key={element.url}>
         <NewsItems  title={element.title} description={element.description?element.description.slice(0,70):""} imageUrl={element.urlToImage?element.urlToImage:""} newsUrl={element.url}/>
             </div>
 
